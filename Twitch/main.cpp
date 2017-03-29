1afac9e4de7ec3e1d7ff47853735d7db2cfb4dba
jellybeanjohnny <mamerige@gmail.com>
2017-03-29 15:59:53 -0700

    Initial Commit

---
+++ Twitch/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Twitch
+//
+//  Created by Matt Amerige on 3/29/17.
+//  Copyright © 2017 Matt Amerige. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
